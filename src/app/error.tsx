9903936a4b522d73e47e3de69694dc8423e6d398
Jasmine <jasminecipollone90@gmail.com>
2024-07-18 17:40:16 +0200

    removing error handler

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,19 +3,19 @@ import { FC } from "react";
 import RedirectButton from "./_components/RedirectButton";
 import "./errorPage.scss";
 
-interface ErrorProps {
+/* interface ErrorProps {
   error: {
     message: string;
   };
-}
+} */
 
-const Error: FC<ErrorProps> = ({ error }) => {
+const Error: FC/* <ErrorProps> */ = (/* { error } */) => {
   return (
     <main className="error-page">
       <h1 className="error-page_wrong">Something went wrong!</h1>
-      <p>{error.message}</p>
+      <p>{/* {error.message} */}You are not authorized to perform this action</p>
 
-      <RedirectButton text="Try Again" redirectUri="/" />
+      <RedirectButton text="Back to Home Page" redirectUri="/" />
     </main>
   );
 };
